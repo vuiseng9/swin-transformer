49a167c92b187aa992ebb213c0b442f0dfb510a5
Vui Seng Chua <vui.seng.chua@intel.com>
2022-09-06 16:59:28 -0700

    Modify config and ft script to reproduce ft-swin-base

--- ft-swin-b-sbatch.sh
+++ ft-swin-b-sbatch.sh
@@ -6,23 +6,23 @@ export WANDB_USERNAME=vchua
 export WANDB_API_KEY=f8a95080288950342f1695008cd8256adc3b0778
 
 # ---------------------------------------------------------------------------------------------
-export WANDB_PROJECT="swin-jpqd"
+export WANDB_PROJECT="sixer-swin"
 
 SWIN_CFG=configs/swin/swin_base_patch4_window7_224_22kto1k_finetune.yaml
-PRETRAINED_CKPT=/data/vchua/run/msft-swin/swin/pretrained/swin_base_patch4_window7_224_22k.pth
+PRETRAINED_CKPT=/data5/vchua/run/test-a100x6/swin/pretrained/swin_base_patch4_window7_224_22k.pth
 
 MASTER_PORT=12345
 BS=128
 
-RUNID=swin-b-p4-w7-r224-22kto1k-ftuned-bs${BS}
+RUNID=swin-b-p4-w7-r224-22kto1k-ftuned-bs${BS}-regression
 
-DATADIR=/data/dataset/imagenet/ilsvrc2012/torchvision/
+DATADIR=/data1/dataset/imagenet/ilsvrc2012/torchvision/
 
-CONDAROOT=/data/vchua/miniconda3
-CONDAENV=msft-swin
+CONDAROOT=/data5/vchua/miniconda3
+CONDAENV=test-a100x6
 
-WORKDIR=/data/vchua/dev/msft-swin/swin-transformer
-OUTROOT=/data/vchua/run/msft-swin/swin
+WORKDIR=/data5/vchua/dev/test-a100x6/swin-transformer
+OUTROOT=/data5/vchua/run/test-a100x6/swin
 
 # ---------------------------------------------------------------------------------------------
 OUTDIR=$OUTROOT/$RUNID

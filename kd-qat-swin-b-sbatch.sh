e7ee2d0bee4d39f6472d795421f984919c592828
Vui Seng Chua <vui.seng.chua@intel.com>
2022-09-08 23:56:27 -0700

    Initial draft of kd-qat

---
+++ kd-qat-swin-b-sbatch.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+export WANDB_DISABLED=false # Enable wandb
+export WANDB_WATCH=false # Disable gradient serialization to wandb
+export WANDB_USERNAME=vchua
+export WANDB_API_KEY=f8a95080288950342f1695008cd8256adc3b0778
+
+# ---------------------------------------------------------------------------------------------
+export WANDB_PROJECT="swin-jpqd"
+
+RUNID=kd-qat-swin-b-22kto1k-tuned
+SWIN_CFG=configs/swin/kd-qat-swin-b-p4-w7-224_22kto1k.yaml
+PRETRAINED_CKPT=/data/vchua/run/msft-swin/swin/swin_base_patch4_window7_224_22kto1k_finetune/default/ckpt_epoch_29.pth
+
+BS=64
+MASTER_PORT=12345
+
+DATADIR=/data/dataset/imagenet/ilsvrc2012/torchvision/
+
+CONDAROOT=/data/vchua/miniconda3
+CONDAENV=msft-swin
+
+WORKDIR=/data/vchua/dev/msft-swin/swin-transformer
+OUTROOT=/data/vchua/run/msft-swin/swin
+# ---------------------------------------------------------------------------------------------
+OUTDIR=$OUTROOT/$RUNID
+mkdir -p $OUTDIR
+cd $WORKDIR
+
+python -m torch.distributed.launch \
+   --nproc_per_node 4 \
+   --master_port $MASTER_PORT \
+   main.py \
+    --cfg $SWIN_CFG \
+    --pretrained $PRETRAINED_CKPT \
+    --data-path $DATADIR \
+    --output $OUTDIR \
+   --batch-size $BS \
+   --wandb_id $RUNID \
+   --accumulation-steps 2
+
+# original config used 8 cards
+# ------------
+
+
+
